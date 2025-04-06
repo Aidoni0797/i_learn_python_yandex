7cec6beb64169e260397f70476c11800ecac69d4
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-04-06 12:22:18 +0600

    21

---
+++ 21.py
@@ -0,0 +1,7 @@
+friends = ['iDONi1', 'iDONi2', 'iDONi3', 'iDONi4', 'iDONi5']
+
+# присвойте переменной index целочисленное значение,
+# чтобы из списка friends была выбрана iDONi4
+index = 3
+
+print('Привет, ' + friends[index] + ', я iDONi!')
