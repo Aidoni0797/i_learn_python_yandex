96802eda804ac34b34130602aaf1a6c68aab30b7
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-04-06 13:04:21 +0600

    30

---
+++ 30.py
@@ -0,0 +1,6 @@
+kids = ['iDONi1', 'iDONi2', 'iDONi3']
+sweets = ['AlpenGold', 'Albeni', 'Oreo', 'Nestle']
+
+for kid in kids:
+    for sweet in sweets:  # Внесите изменения в строку.
+        print(kid, 'получает конфету', sweet)  # Внесите изменения в строку.
