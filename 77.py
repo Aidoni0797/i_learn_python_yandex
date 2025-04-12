cfff05d18a51f8326d09b8ebb50cf96f219911c9
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-04-13 01:01:55 +0600

    77

---
+++ 77.py
@@ -0,0 +1,10 @@
+def print_home(name, planet):
+    print(name + ' живет на планете ' + planet)
+
+tatooin = 'Татуин'
+greeting = 'Да пребудет с тобой Сила!'
+my_son = 'Люк, я твой отец!'
+luke = 'Люк Скайуокер'
+
+# Вызов функции
+print_home(luke, tatooin) 
