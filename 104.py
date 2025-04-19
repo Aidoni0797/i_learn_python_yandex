fa5694aca9753147d326aba3f1ee3ef5f1039950
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-04-19 18:59:20 +0600

    104

---
+++ 104.py
@@ -0,0 +1,8 @@
+def multiplication(multiplier_1, multiplier_2):
+    print(multiplier_1 * multiplier_2)
+
+# Дальше начинается код, который расположен вне функции:
+# Python понимает это по отсутствию отступов.
+
+# Вызов функции multiplication()
+multiplication(7, 6)
