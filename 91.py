e2ef2497682330a526f092a288f8cab4b9a234aa
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-04-19 16:51:55 +0600

    91

---
+++ 91.py
@@ -0,0 +1,20 @@
+counter = 0
+countdown = 0
+money = 0
+problem = 0
+
+# Например, вместо строки
+counter = counter + 10  # На каждом шаге counter увеличивается на десять
+# можно написать
+counter += 10  # На каждом шаге counter увеличивается на десять
+
+# Подобный оператор есть и для вычитания
+countdown -= 3  # На каждом шаге countdown уменьшается на три
+
+# и для умножения
+money *= 7  # На каждом шаге money умножаются на 7
+
+# и для деления
+problem /= 2  # На каждом шаге problem делятся пополам
+
+print(counter, countdown, money, problem)
