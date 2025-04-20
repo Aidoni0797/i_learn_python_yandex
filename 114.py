e521e0106c709d4a81d4e935759884936307680a
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-04-20 10:39:26 +0600

    114

---
+++ 114.py
@@ -0,0 +1,13 @@
+playlist = {
+    'Venus',
+    'Yesterday',
+    'Fireball',
+    'Time',
+    'Poison'
+}
+
+playlist.add('Thunderstruck')
+print(playlist)
+# Будет напечатано:
+# {'Yesterday', 'Fireball', 'Thunderstruck', 'Poison', 'Venus', 'Time'}
+# Элементы множеств никогда не соблюдают порядок!
